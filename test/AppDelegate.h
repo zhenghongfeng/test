6922438a7e0ceb2c118f7554b2487ac5c9c5c452
zhenghongfeng <zhenghongfeng@mail.open.com.cn>
2019-01-04 14:34:31 +0800

    xxx

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by hongfeng on 2019/1/4.
+//  Copyright © 2019 hongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by hongfeng on 2019/1/4.
+//  Copyright © 2019 hongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
